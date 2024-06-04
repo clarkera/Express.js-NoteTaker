64b86132bb595b4e98fe8fa899e0599b62088303
clarkera <rclarke9941@gmail.com>
2024-06-04 13:01:40 -0400

    updates

---
+++ server.js
@@ -0,0 +1,25 @@
+// Imports express
+const express = require('express');
+const path = require('path');
+
+// Creates an instance of express
+const app = express()
+
+// Set Port
+const PORT = process.env.PORT || 3001
+
+// Use app to create a public folder /public/, this will be the default landing page
+app.use(express.static('public'));
+
+// Middleware to set up express app across webApp
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes to route files
+require('./routes/apiRoutes')(app);
+require('./routes/index')(app);
+
+// Listener for port 3001, displays a console log to show activation
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
